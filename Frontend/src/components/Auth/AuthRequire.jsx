9cd7ca5c4aff1e081a7f297d4bcc0a76b79c4959
Anshuman369 <78948391+AnshumanPatek@users.noreply.github.com>
2025-01-25 03:49:05 +0530

    fix: backend

--- Frontend/src/components/Auth/AuthRequire.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom";
-export const RequiredAuth = ({children}) =>{
-    const user = localStorage.getItem('user');
-    if(!user)
-        return <Navigate to='/login'/>
-
-    return children;
-
-}
